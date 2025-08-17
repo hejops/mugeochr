4e8b38925e3471b8ebd9ccbbcfc900882c1cb856
Joseph <hejops1@gmail.com>
2025-08-17 18:31:02 +0200

    fix: declare base

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/mugeochr/", // must match repo name!
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
